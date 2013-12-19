f95334aef7234c0a5cf484c46bf43dfb2e3d8804
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-19 18:34:57 +0400

    errors\Error: Basic error of axy\magic

---
+++ errors/Error.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package axy\magic
+ */
+
+namespace axy\magic\errors;
+
+/**
+ * Basic error of axy\magic
+ *
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+interface Error extends \axy\errors\Error
+{
+}
